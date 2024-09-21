97b959ca31e2e1daf1375b2f3b8981f15bf1f7af
yue ye <1650958692@qq.com>
2024-09-21 21:39:26 +0800

    latex

--- yt_streamlit.py
+++ yt_streamlit.py
@@ -71,10 +71,3 @@ if coffee:
 if cola:
      st.write("Here you go 🥤")
 
-st.subheader('st.latex')
-
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
