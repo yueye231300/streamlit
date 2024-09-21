80a0852ce3352d0a9bc0c7142201dd86a43cf044
yue ye <1650958692@qq.com>
2024-09-21 22:19:28 +0800

    pprogress

--- yt_streamlit.py
+++ yt_streamlit.py
@@ -123,6 +123,7 @@ with col3:
      else:
           st.write('👈 Please choose your favorite **food**!')
 
+
 with st.expander('About this app'):
      st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
 
