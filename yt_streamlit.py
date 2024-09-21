8e2cc6682ddbe274b01c6d6ade09798644d2048a
yue ye <1650958692@qq.com>
2024-09-21 21:59:44 +0800

    layout

--- yt_streamlit.py
+++ yt_streamlit.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 
-
+st.set_page_config(layout="wide")
 st.header('st.slider')
 
 st.subheader('Slider')
@@ -94,8 +94,6 @@ number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)
 
 
-st.set_page_config(layout="wide")
-st.title('How to layout your Streamlit app')
 with st.expander('About this app'):
      st.write('This app shows the various ways on how you can layout your Streamlit app.')
      st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
