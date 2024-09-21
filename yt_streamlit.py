ff343cdaa63933f4b3e239ccb3925e1494bc51b4
yue ye <1650958692@qq.com>
2024-09-21 21:46:01 +0800

    color

--- yt_streamlit.py
+++ yt_streamlit.py
@@ -78,3 +78,17 @@ st.latex(r'''
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
